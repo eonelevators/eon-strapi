2f4f0a0b5ba7c444391ea791b69e934d71a1dc06
Vishmitha V Poojary <vishumitha98809@gmail.com>
2026-01-24 17:12:43 +0530

    creating product page schema for hero section

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,28 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SectionsHero extends Struct.ComponentSchema {
+  collectionName: 'components_sections_heroes';
+  info: {
+    displayName: 'hero';
+    icon: 'star';
+  };
+  attributes: {
+    cta_link: Schema.Attribute.String;
+    cta_text: Schema.Attribute.String;
+    heading: Schema.Attribute.String;
+    hero_description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    subheading: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'sections.hero': SectionsHero;
+    }
+  }
+}
